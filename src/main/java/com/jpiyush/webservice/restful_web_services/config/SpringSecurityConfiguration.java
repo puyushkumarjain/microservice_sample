dc4ae8781703e4b95d0d74da8232d183ae135784
puyushkumarjain <piyush.jain1328@gmail.com>
2026-09-02 17:09:46 +0530

    Basic Implmented SpringSecurityConfiguration

---
+++ src/main/java/com/jpiyush/webservice/restful_web_services/config/SpringSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.jpiyush.webservice.restful_web_services.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+//        http.authorizeHttpRequests(authz -> authz
+//                .requestMatchers("/users/**").authenticated()
+//                .anyRequest().permitAll()
+//        );
+
+//		1) All requests should be authenticated
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+//		2) If a request is not authenticated, a web page is shown
+        http.httpBasic(withDefaults());
+
+//		3) CSRF -> POST, PUT
+        // http.csrf().disable(); // Deprecated in SB 3.1.x
+        http.csrf(AbstractHttpConfigurer::disable); // Starting from SB 3.1.x Using Lambda DSL
+        // OR
+        // http.csrf(AbstractHttpConfigurer::disable); // Starting from SB 3.1.x Using Method Reference
+
+        return http.build();
+    }
+}
