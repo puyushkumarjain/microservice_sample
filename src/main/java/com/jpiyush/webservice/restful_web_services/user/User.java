65d89997735703b54ef802a85e204072584f0585
puyushkumarjain <piyush.jain1328@gmail.com>
2026-09-02 14:30:52 +0530

    Basic User and post relationship

--- src/main/java/com/jpiyush/webservice/restful_web_services/user/User.java
+++ src/main/java/com/jpiyush/webservice/restful_web_services/user/User.java
@@ -1,22 +1,46 @@
 package com.jpiyush.webservice.restful_web_services.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity(name = "user_details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
     @Past(message = "Birth date should be in the past")
     private LocalDate birthDate;
 
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
+
+    public User() {
+    }
+
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     public Integer getId() {
